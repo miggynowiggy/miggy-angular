fe614de8c28840c7eb6e732db2933bfb0d3c1a8d
Miggy Mendoza <mendozajuanmiguel11@gmail.com>
2020-07-11 15:51:10 +0800

    finalized about-page component

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Skill } from './../skill';
-
-declare let AOS: any;
 
 @Component({
   selector: 'app-about-page',
@@ -9,21 +6,12 @@ declare let AOS: any;
   styleUrls: ['./about-page.component.scss']
 })
 export class AboutPageComponent implements OnInit {
-  skills: Array<Skill>;
+  bgURL: string;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
-    this.skills  = [
-      new Skill('HTML', 'fab fa-html5', '#F06529', 'Proficient', 'html5'),
-      new Skill('CSS', 'fab fa-css3', '#0780C7', 'Proficient', 'CSS3'),
-      new Skill('JavaScript', 'fab fa-js-square', '#FBD601', 'Expert', 'JS'),
-      new Skill('Vue', 'fab fa-vuejs', '#41b883', 'Expert', 'VUE'),
-      new Skill('React', 'fab fa-react', '#00D9FF', 'Intermmediate', 'REACT'),
-      new Skill('Node.js', 'fab fa-node-js', '#00D9FF', 'Advance', 'NODE'),
-      new Skill('Firebase', 'fas fa-server', '#00D9FF', 'Expert', 'FIREBASE'),
-    ];
-
-    AOS.init();
-
   }
 
 }
