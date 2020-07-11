151e978a361cedf7155c9b717775db2f514f75d9
Miggy Mendoza <mendozajuanmiguel11@gmail.com>
2020-07-11 22:47:05 +0800

    finalized all pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,26 +8,31 @@ import { AngularFireStorageModule, BUCKET } from '@angular/fire/storage';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { AboutPageComponent } from './about-page/about-page.component';
+import { SkillsPageComponent } from './skills-page/skills-page.component';
 import { WorksPageComponent } from './works-page/works-page.component';
-
-import { environment } from '../environments/environment';
+import { AboutPageComponent } from './about-page/about-page.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ViewWorkPageComponent } from './view-work-page/view-work-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+import { environment } from '../environments/environment';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContactsPageComponent } from './contacts-page/contacts-page.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    AboutPageComponent,
+    SkillsPageComponent,
     WorksPageComponent,
     MainPageComponent,
     ViewWorkPageComponent,
     PageNotFoundComponent,
     NavBarComponent,
+    AboutPageComponent,
+    ContactsPageComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/work.ts
@@ -0,0 +1,7 @@
+export class Work {
+    constructor(
+        public title: string,
+        public downloadURL: string,
+        public description: string
+    ) { }
+}
